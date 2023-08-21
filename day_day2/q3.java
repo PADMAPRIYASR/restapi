235200a91bb89ea08e8305d668709e4eb2a0c929
PADMAPRIYASR <108685209+PADMAPRIYASR@users.noreply.github.com>
2023-08-21 16:24:28 +0530

    Add files via upload

---
+++ day_day2/q3.java
@@ -0,0 +1,16 @@
+package com.example.demo.day_day1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class q3 {
+	@Value("Sage Green")
+	 public String name;
+	@RequestMapping("/colour")
+	public String colour(){
+	return "My Favourite colour is "+name;
+}
+
+}
